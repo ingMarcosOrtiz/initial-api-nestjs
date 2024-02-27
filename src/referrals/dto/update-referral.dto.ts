b44f1d3a5d6e8145addd752c3ebacce768884e6a
Marcos Ortiz <ingmarcosortiz@gmail.com>
2024-02-27 14:39:53 -0500

    remove referrals

--- src/referrals/dto/update-referral.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateReferralDto } from './create-referral.dto';
-
-export class UpdateReferralDto extends PartialType(CreateReferralDto) {}
